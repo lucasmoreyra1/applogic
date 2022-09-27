4ec4f7371052a9aab2a2599883f27ec0a75f0a6d
Benjamin Vazquez <105523308+BenjaPr0gramer@users.noreply.github.com>
2022-09-27 18:20:25 -0300

    Media query para android y index distinto

--- php-login/index.php
+++ php-login/index.php
@@ -26,35 +26,32 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>UBITEC</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;1,700&display=swap" rel="stylesheet">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Mapa</title>
+		<link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
+		integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
+		crossorigin=""/>
         <link rel="Stylesheet" href="assets/css/style.css">
         <style>
-        .direccion{
-            margin: 20px;
-        }
-        input[type="text"]{
-            height: 15px;
-        }
-        table{
-            background: white;
-            border: 1px solid black;
-            width: 500px;
-            margin: auto;
-        }
-        th,td{
-            border: 1px solid black;
-            text-align: center;
-        }
+			table{
+				background: #025669;
+				color: white;
+				border: 1px solid black;
+				width: 470px;
+			}
+			th,td{
+				border: 1px solid black;
+				text-align: center;
+				padding: 10px;
+			}
         </style>
     </head>
-    <body>
-
+    <body class="fondo1">
+			
         <?php
             require 'partials/header.php';
         ?>
@@ -65,22 +62,39 @@
             <p>you are sussesfully loged in</p>
             <a href="logout.php">logoout</a>
         <?php else: ?>
-		<article class="caja">
+		<article class="caja2">
             <h1>Por favor entre o registrese para continuar</h1>
-            <a href="login.php"><input type="button" value="Logearse"></a>
-            <a href="signup.php"><input type="button" value="Registrarse"></a>
+            <a class="boton" href="login.php"><input type="button" value="Logearse"></a>
+            <a class="boton" href="signup.php"><input type="button" value="Registrarse"></a>
         <?php endif ?>
         
         <form method="post">
-            <div class="form-group">
-                <div class="direccion">
-                    <input type="text" name="searchAddress" id="id" placeholder="Por favor ingrese la direccion">
-                    <input type="hidden" name="valores" value="<?php echo implode(",", $_SESSION['data']); ?>">
-                    <input type="submit" id="add" value="Añadir">
-                </div>
+            <div>
+                <input type="text" name="searchAddress" id="id" placeholder="Por favor ingrese la direccion">
+                <input type="hidden" name="valores" value="<?php echo implode(",", $_SESSION['data']); ?>">
             </div>
+                <input type="submit" id="add" value="Añadir">
         </form>
 		</article>
+			<div id="map"></div>
+			<script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"
+			   integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ=="
+			   crossorigin=""></script>
+		    <script src="map.js"></script>
+			<script src="https://unpkg.com/esri-leaflet@2.4.1/dist/esri-leaflet.js"
+			   integrity="sha512-xY2smLIHKirD03vHKDJ2u4pqeHA7OQZZ27EjtqmuhDguxiUvdsOuXMwkg16PQrm9cgTmXtoxA6kwr8KBy3cdcw=="
+			   crossorigin=""></script>
+		    <!-- Load Esri Leaflet Geocoder from CDN -->
+		    <link rel="stylesheet" href="https://unpkg.com/esri-leaflet-geocoder@2.3.3/dist/esri-leaflet-geocoder.css"
+			 integrity="sha512-IM3Hs+feyi40yZhDH6kV8vQMg4Fh20s9OzInIIAc4nx7aMYMfo+IenRUekoYsHZqGkREUgx0VvlEsgm7nCDW9g=="
+			 crossorigin="">
+		    <script src="https://unpkg.com/esri-leaflet-geocoder@2.3.3/dist/esri-leaflet-geocoder.js"
+			 integrity="sha512-HrFUyCEtIpxZloTgEKKMq4RFYhxjJkCiF5sDxuAokklOeZ68U2NPfh4MFtyIVWlsKtVbK5GD2/JzFyAfvT5ejA=="
+			 crossorigin=""></script> 
+			 <!-- Load Esri Leaflet Vector from CDN -->
+			 <script src="https://unpkg.com/esri-leaflet-vector@4.0.0/dist/esri-leaflet-vector.js"
+			 integrity="sha512-EMt/tpooNkBOxxQy2SOE1HgzWbg9u1gI6mT23Wl0eBWTwN9nuaPtLAaX9irNocMrHf0XhRzT8B0vXQ/bzD0I0w=="
+			 crossorigin=""></script>
 				<table>
                     <?php
                         echo count($valores);
@@ -90,12 +104,11 @@
                         }
                     ?>
                 </table>
+		
         <!--Reloj v1, solo se activa al cargar la pagina-->
-        
-        
-        <div>
-      <div class="tiempo" id="tiempo">0:00:00</div>
-      </div>
+        <!-- <div>
+			<div class="tiempo" id="tiempo">0:00:00</div>
+		</div>
 
         <script>
 
@@ -128,7 +141,7 @@
 
           return H.ceros(1) + ":" + M.ceros(2) + ":" + S.ceros(2)
       }
-    </script>
+		</script>-->
 
 
     </body>
