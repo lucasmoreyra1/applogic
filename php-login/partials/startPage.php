6a117d43b42efa0425bf31345536d0bb89d02d12
Lucas Moreyra <lucasmoreyra1703@gmail.com>
2022-10-30 04:43:22 -0300

    Nueva pagina de inicio

--- php-login/partials/startPage.php
+++ php-login/partials/startPage.php
@@ -17,23 +17,38 @@
     <?php require 'header.php'; ?>
     <section class="hero">
         <div class="contenido-hero">
-            <h2>Soluciones de<span> logistica</span></h2>
 
 
-            <p>
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-pin" width="80" height="80" viewBox="0 0 24 24" stroke-width="1.5" stroke="#c9de00" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <circle cx="12" cy="11" r="3" />
                     <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
                 </svg>
-                San Nicolas de los Arroyos</p>
-            <a class="boton" href="#">Contactar</a>
+                <h2>Soluciones de<span> logistica</span></h2>
+
+            <div class="contenedor">
+                <div class="hijo">
+                    <a href="../login.php"><input class="boton" type="button"  value="Entrar"></a>
+                    <a href="../signup.php"><input class="boton" type="button" value="Registrarse"></a>
+                </div>
+            </div>
+
 
         </div>
     </section>
     <main class="contenedor sombra">
         <section>
             <h2>Quienes somos</h2>
+            <p><span>UBITEC</span> es una empresa dedicada a prestar servicios para facilitar el recorrido de rutas
+             a transportistas, repartidores y la industria de la logistica.</p>
+            <p>Nuestro servicio es uno de los mas rapidos y mejores del mercado, ofreciendo una solucion eficaz para empresas de pequeño y gran tamaño.</p>
+            
+            <h2>A quienes nos dirigimos</h2>
+            <p><span>UBITEC</span> es una empresa dedicada a prestar servicios para facilitar el recorrido de rutas
+             a transportistas, repartidores y la industria de la logistica.</p>
+            <p>Nuestro servicio es uno de los mas rapidos y mejores del mercado, ofreciendo una solucion eficaz para empresas de pequeño y gran tamaño.</p>
+
+            <h2>....</h2>
             <p><span>UBITEC</span> es una empresa dedicada a prestar servicios para facilitar el recorrido de rutas
              a transportistas, repartidores y la industria de la logistica.</p>
             <p>Nuestro servicio es uno de los mas rapidos y mejores del mercado, ofreciendo una solucion eficaz para empresas de pequeño y gran tamaño.</p>
